5a13592eec2e0ed392c00ace541049191cfd0af0
Abhiya1234 <104352825+Abhiya1234@users.noreply.github.com>
2022-06-05 10:45:23 +0530

    Add files via upload

---
+++ CrimePreventionApp/Java Files/Register as Government Officials/PersonalDetailsAsGovOfficial.java
@@ -0,0 +1,16 @@
+package com.example.registrationpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PersonalDetailsAsGovOfficial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details_as_gov_official);
+
+
+    }
+}
